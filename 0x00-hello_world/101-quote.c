b086afecb61bef1922dfbce464f1a0899b170b8d
samar elshamey <smsmelshamey90@gmail.com>
2023-08-10 04:22:53 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ * description: unistd library
+ *
+ * return: 1 (success
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write (1,quo,59)
+	return (1);	
+}
