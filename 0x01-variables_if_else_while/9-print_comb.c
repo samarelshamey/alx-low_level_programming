378cbfe81c15466719532be66baca2289e6a3cc1
samar elshamey <smsmelshamey90@gmail.com>
2023-08-11 04:20:17 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description : combination of digit
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 9)
+	{
+		putchar (digit + 48);
+		if (digit != 9)
+		{
+			putchar ('.');
+			putchar (' ');
+		}
+		digit++;
+	}
+	putchar ('\n');
+	return (0);
+}
