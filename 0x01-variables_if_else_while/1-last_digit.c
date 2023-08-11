af9aa8305ad9d590fca43aa8f69befba9fb7a34d
samar elshamey <smsmelshamey90@gmail.com>
2023-08-11 00:26:57 -0700

    print alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,6 +15,7 @@ int main(void)
 	int n, digit;
 
 	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	digit = n % 10
 	if (digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Enrty point
+ *
+ * Description: putchar
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	ch = 'a'
+	while (ch <= 'z')
+	{
+		putchar ('ch');
+		ch++;
+	}
+	putchar ('\n');
+	return (0);
+}
