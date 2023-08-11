a873dd8d6e230d22616fb6dbe56717a5d374bc4b
samar elshamey <smsmelshamey90@gmail.com>
2023-08-11 05:04:48 -0700

    comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: combination of 3 numbers
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int dig1 = 0;
+	int dig2, dig3;
+
+	while (dig1 <= 9)
+	{
+		dig2 = 0;
+		while (dig1 <= 9)
+		{
+			dig3 = 0;
+			while (dig3 <= 9)
+			{
+				if (dig1 != dig2 &&
+					dig1 < dig2 &&
+					dig2 != dig3 &&
+					dig2 < dig3)
+				{
+					putchar (dig1 + 48);
+					putchar (dig2 + 48);
+					putchar (dig3 + 48);
+					if (dig1 + dig2 + dig3 != 24)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+		dig3++;
+			}
+	dig2++;
+		}
+	dig1++;
+	}
+	putchar ('\n');
+	return (0);
+}
