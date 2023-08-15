636aac720455a0c771ac3033081400046e27592a
samar elshamey <smsmelshamey90@gmail.com>
2023-08-15 12:27:06 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - function used to print  the 9 times table
+*/
+
+void times_table(void)
+{
+	int i, multi, result;
+
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar(48);
+		for (multi = 0; multi <= 9; multi++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			result = i * multi;
+			if (result <= 9)
+				_putchar(' ');
+			else
+				_putchar((result / 10) + 48);
+			_putchar((result % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
