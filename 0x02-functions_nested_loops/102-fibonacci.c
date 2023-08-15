42679a905ca43b7bfb900bd1704e01ab7af724b9
samar elshamey <smsmelshamey90@gmail.com>
2023-08-15 15:38:30 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - etry point
+ *
+ * Description: prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int count;
+	unsigned long x = 0, y = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = x + y;
+			printf("%lu", sum);
+		x = y;
+		y = sum;
+		if (count == 49)
+			printf("\n");
+		else
+			printf(",");
+	}
+	return (0);
+}
