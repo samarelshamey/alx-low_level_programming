b523fc8d3f8eed41ad12fc4e4c1935c7dfd64430
samar elshamey <smsmelshamey90@gmail.com>
2023-08-15 08:55:54 -0700

    putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function used to print alphabet 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch < 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
