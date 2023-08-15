6aa059c4bb91227f0200ccc66256510600720550
samar elshamey <smsmelshamey90@gmail.com>
2023-08-15 13:13:31 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function to add two integrs
+ * @x : input
+ *
+ * @y : input
+ *
+ * Return : sum
+*/
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
