299e3c97ec4b02cc59f68c9a897b149186152a1c
samar elshamey <smsmelshamey90@gmail.com>
2023-08-17 07:48:06 -0700

    functions and nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,12 +16,15 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (a = 1; a >= size; a++)
+		for (a = 1; a <= size; a++)
 		{
-			for (b = 1; b >= size; b++)
+			for (b = a; b < size; b++)
 			{
-				if ((a + b) <= size)
-					_putchar(' ');
+				_putchar(' ');
+			}
+			for (b = 1; b <= a; b++)
+			{
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
