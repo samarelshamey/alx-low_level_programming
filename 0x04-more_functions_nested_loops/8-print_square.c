3291b66e5acf14716733494611a73ea4d45e845f
samar elshamey <smsmelshamey90@gmail.com>
2023-08-17 06:17:32 -0700

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - function
+ *
+ * @size: square size
+*/
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('#');
+	}
+}
