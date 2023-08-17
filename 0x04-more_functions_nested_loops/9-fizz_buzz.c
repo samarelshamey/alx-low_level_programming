c5ad8b1951b0cdd2c9f8d0a6272dffaec809c8bc
samar elshamey <smsmelshamey90@gmail.com>
2023-08-17 07:04:55 -0700

    functions and nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,26 +15,18 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0)
-		{
 			printf("Fizz");
 			printf(" ");
-		}
 		else if (n % 5 == 0)
-		{
 			printf("Buzz");
 			printf(" ");
-		}
 		else if (n % 5 == 0 && n % 3 == 0)
-		{
 			printf("FizzBuzz ");
 			printf(" ");
-		}
 		else
-		{
 			printf("%d ", n);
 			printf(" ");
-		}
 	}
-		printf("\n");
-	return 0;
+	printf("\n");
+	return (0);
 }
