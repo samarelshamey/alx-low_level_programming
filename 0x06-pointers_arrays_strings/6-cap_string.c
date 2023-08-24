a150a9eaf4ad2545cfa84c7b9b7edd39a717754b
samar elshamey <smsmelshamey90@gmail.com>
2023-08-24 05:11:36 -0700

    pointers & arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,65 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * isLower - function to lowercase
+ *
+ * Description: function to lowercase
+ *
+ * @c: character
+ *
+ * Return: character
+*/
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - function
+ *
+ * @c : char
+ *
+ * Return: index
+*/
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \n\t,;.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (i);
+	return (0);
+}
 
 /**
  * cap_string - function to capitalize words
  *
  * @s: string
  *
- * Return: char
+ * Return: pointer
 */
 
 char *cap_string(char *s)
 {
-	int i = 0;
-	int cap_next = 1;
-	char separators[] = " \n\t,;.!?\"(){}";
-	char *sep = separators;
+	char *ptr = s;
+	int foundDelimit = 1;
 
-	while (s[i] != '\0')
+	while (*s)
 	{
-		if (s[i] == *sep)
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
 		{
-			cap_next = 1;
+			*s -= 32;
+			foundDelimit = 0;
 		}
-		else if (cap_next)
-		{
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				s[i] = s[i] - 'a' + 'A';
-			}
-			cap_next = 0;
-		}
-		i++;
+		else
+			foundDelimit = 0;
+		s++;
 	}
-	return (s);
+	return (ptr);
 }
