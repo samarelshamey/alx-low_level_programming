46e35b04ce63025cfb450c99ee170a8d1135c739
samar elshamey <smsmelshamey90@gmail.com>
2023-08-24 03:05:13 -0700

    pointers & arrays and strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - fnction to print string in uppercase
+ *
+ * @s: string
+ *
+ * Return: char
+*/
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] > 'a' && s[i] < 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+		i++;
+	}
+	return (s);
+}
