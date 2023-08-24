68c07fd7494bcf08b779a03759218794fffa206a
samar elshamey <smsmelshamey90@gmail.com>
2023-08-24 05:55:29 -0700

    pointers & arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,65 +1,42 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * isLower - function to lowercase
- *
- * Description: function to lowercase
- *
- * @c: character
- *
- * Return: character
-*/
-
-int isLower(char c)
-{
-	return (c >= 97 && c <= 122);
-}
-
-/**
- * isDelimiter - function
- *
- * @c : char
- *
- * Return: index
-*/
-
-int isDelimiter(char c)
-{
-	int i;
-	char delimiter[] = " \n\t,;.!?\"(){}";
-
-	for (i = 0; i < 12; i++)
-		if (c == delimiter[i])
-			return (i);
-	return (0);
-}
 
 /**
  * cap_string - function to capitalize words
  *
  * @s: string
  *
- * Return: pointer
+ * Return: ptr
 */
 
 char *cap_string(char *s)
 {
-	char *ptr = s;
-	int foundDelimit = 1;
+	int cap_next = 1;
 
-	while (*s)
+	for (int i = 0; s[i] != '\0'; i++)
 	{
-		if (isDelimiter(*s))
-			foundDelimit = 1;
-		else if (isLower(*s) && foundDelimit)
+		if (cap_next && s[i] >= 'a' && s[i] <= 'z')
 		{
-			*s -= 32;
-			foundDelimit = 0;
+			s[i] -= 32;
 		}
-		else
-			foundDelimit = 0;
-		s++;
+		cap_next = 0;
+		switch (s[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				cap_next = 1;
+				break;
+		}
 	}
-	return (ptr);
+	return (s);
 }
