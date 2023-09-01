85b979b32a8ba870f0a746e936a0410375a0298f
samar elshamey <smsmelshamey90@gmail.com>
2023-09-01 05:38:55 -0700

    static library

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: arguments
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
