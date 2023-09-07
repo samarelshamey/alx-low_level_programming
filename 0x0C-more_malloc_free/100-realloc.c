98f5ed46c32dbbf58a96805e7fa5ea7778d8bed6
samar elshamey <smsmelshamey90@gmail.com>
2023-09-07 16:24:05 -0700

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	int *new_ptr;
 	unsigned int min_size;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
