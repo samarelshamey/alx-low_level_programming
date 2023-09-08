493196c31504b135856acde90a1c1b710a3a2721
samar elshamey <smsmelshamey90@gmail.com>
2023-09-08 05:33:11 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -39,22 +39,22 @@ int _strlen(char *s)
  */
 char *big_multiply(char *s1, char *s2)
 {
-	char *r;
+	char *ptr;
 	int l1, l2, a, b, c, x;
 
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
+	ptr = malloc(a = x = l1 + l2);
+	if (!ptr)
 		printf("Error\n"), exit(98);
 	while (a--)
-		r[a] = 0;
+		ptr[a] = 0;
 
 	for (l1--; l1 >= 0; l1--)
 	{
 		if (!_isdigit(s1[l1]))
 		{
-			free(r);
+			free(ptr);
 			printf("Error\n"), exit(98);
 		}
 		a = s1[l1] - '0';
@@ -64,20 +64,20 @@ char *big_multiply(char *s1, char *s2)
 		{
 			if (!_isdigit(s2[l2]))
 			{
-				free(r);
+				free(ptr);
 				printf("Error\n"), exit(98);
 			}
 			b = s2[l2] - '0';
 
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
+			c += ptr[l1 + l2 + 1] + (a * b);
+			ptr[l1 + l2 + 1] = c % 10;
 
 			c /= 10;
 		}
 		if (c)
-			r[l1 + l2 + 1] += c;
+			ptr[l1 + l2 + 1] += c;
 	}
-	return (r);
+	return (ptr);
 }
 
 
@@ -90,27 +90,27 @@ char *big_multiply(char *s1, char *s2)
  */
 int main(int argc, char **argv)
 {
-	char *r;
-	int a, c, x;
+	char *ptr;
+	int y, z, x;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
 
 	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
+	ptr = big_multiply(argv[1], argv[2]);
+	z = 0;
+	y = 0;
+	while (z < x)
 	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
+		if (ptr[z])
+			y = 1;
+		if (y)
+			_putchar(ptr[z] + '0');
+		z++;
 	}
-	if (!a)
+	if (!y)
 		_putchar('0');
 	_putchar('\n');
-	free(r);
+	free(ptr);
 	return (0);
 }
