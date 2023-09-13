ae3d3075c43367426abd46c53fd1488b7f783098
samar elshamey <smsmelshamey90@gmail.com>
2023-09-13 15:42:10 -0700

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,16 +14,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
-	}
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
 	return (-1);
-	if (size <= 0)
-	{
-		return (-1);
-	}
 }
