4f05b9d1a5af167c6b62110e897b8f348939be1f
samarelshamey <smsmelshamey90@gmail.com>
2023-09-21 15:19:57 -0700

    single linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_message - function to print specific msg
+ *
+ * Return: nothing
+*/
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
