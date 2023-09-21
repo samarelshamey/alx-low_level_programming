3be1e0041e2fc4ebfa295d9e54fb12948ceb3e68
samarelshamey <smsmelshamey90@gmail.com>
2023-09-21 14:05:50 -0700

    single linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * *add_node - function to add node
+ *
+ * @head: head
+ *
+ * @str: data
+ *
+ * Return: integer
+*/
+
+list_t *add_node(list_t **head, const char *str);
+{
+	list_t *ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr->str = strdup(str);
+	if (ptr->str == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->len = strlen(str);
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
+}
