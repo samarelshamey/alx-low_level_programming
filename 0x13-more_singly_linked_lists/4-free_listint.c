d37889a04f59255b3e2230174409f33d19ae8e4b
samarelshamey <smsmelshamey90@gmail.com>
2023-09-25 15:46:30 -0700

    more singly linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ *
+ * @head: head
+ *
+ * Return: nothing
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
