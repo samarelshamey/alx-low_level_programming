db6a85e6072f25773dc6c05e5648ba6b3aa1c25a
samarelshamey <smsmelshamey90@gmail.com>
2023-09-26 09:59:29 -0700

    more singly linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print function
+ *
+ * @head: head
+ *
+ * Return: integer
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *temp = head;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		count++;
+		if (temp <= temp->next)
+		{
+			printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
+			break;
+		}
+		temp = temp->next;
+	}
+	return (count);
+}
