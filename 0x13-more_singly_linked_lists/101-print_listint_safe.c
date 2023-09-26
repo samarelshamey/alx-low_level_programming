73c5b4b9fec9429a0499d7a9e4ff978a9d208baf
samarelshamey <smsmelshamey90@gmail.com>
2023-09-26 12:15:39 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,43 +9,20 @@
 */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *s = head, *f = head;
+	const listint_t *current = head;
 	size_t count = 0;
 
-	while (f != NULL && f->next != NULL)
+	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)s, s->n);
+		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
-		s = s->next;
-		f = f->next->next;
-		if (s == f)
+
+		if (current->next >= current)
 		{
-			printf("[%p] %d", (void *)s, s->n);
-			count++;
-			break;
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			exit(98);
 		}
-	}
-	if (f == NULL || f->next == NULL)
-	{
-		while (s != NULL)
-		{
-			printf("[%p] %d\n", (void *)s, s->n);
-			count++;
-			s = s->next;
-		}
-	}
-	else
-	{
-		s = head;
-		while (s != f)
-		{
-			printf("[%p] %d\n", (void *)s, s->n);
-			count++;
-			s = s->next;
-			f = f->next;
-		}
-		printf("[%p] %d\n", (void *)s, s->n);
-		count++;
+		current = current->next;
 	}
 	return (count);
 }
