04dd3b7134dfe7bfaa9c9ab631cc273a34fa6a2b
samarelshamey <smsmelshamey90@gmail.com>
2023-09-26 05:30:03 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	idx--;
 
-	while (idx != 0 || temp != NULL)
+	while (idx != 0 && temp != NULL)
 	{
 		temp = temp->next;
 		idx--;
