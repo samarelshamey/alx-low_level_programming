88b2cb354607451523333b1409096403f8889e3a
samarelshamey <smsmelshamey90@gmail.com>
2023-09-26 10:04:51 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,8 +12,6 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *temp = head;
 	size_t count = 0;
 
-	if (temp == NULL)
-		exit(98);
 	while (temp != NULL)
 	{
 		printf("[%p] %d\n", (void *)temp, temp->n);
