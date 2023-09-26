7ae62733e43ef9b64bedff495fcd0f13a26d2dab
samarelshamey <smsmelshamey90@gmail.com>
2023-09-26 12:33:39 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,63 @@
 #include "lists.h"
 
 /**
+ * _reallocate - reallocate memory
+ *
+ * @list: list
+ *
+ * @size: size
+ *
+ * @new: new list
+ *
+ * Return: integer
+*/
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
  * print_listint_safe - print function
  *
  * @head: head
  *
  * Return: integer
 */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
-	size_t count = 0;
+	size_t i, count = 0;
+	const listint_t **list = NULL;
 
-	while (current != NULL)
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-
-		if (current->next >= current)
+		for (i = 0; i < count; i++)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			exit(98);
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (count);
+			}
 		}
-		current = current->next;
+		count++;
+		list = _r(list, count, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
+	free(list);
 	return (count);
 }
