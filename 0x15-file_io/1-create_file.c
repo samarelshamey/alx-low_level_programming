6eb1839110214b4df795d225c34906155f2d4ae5
samarelshamey <smsmelshamey90@gmail.com>
2023-10-02 09:40:04 -0700

    i/o project

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - length of string
+ *
+ * @s: string
+ *
+ * Return: integer
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
  * create_file - creates file
  *
  * @filename: name of the file
