e6862f85d4f79e46cdc8b037e4b974781e76737f
samarelshamey <smsmelshamey90@gmail.com>
2023-10-30 04:50:23 -0700

    file i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfile - function read and print text file
+ *
+ * @filename: file name
+ *
+ * @letters: letters in the file
+ *
+ * Return: intger
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	char ch[letters + 1];
+	size_t bytesread, byteswritten;
+
+	if (filename == NULL)
+		return (0);
+	file = fopen(filename, "r");
+	if (file == NULL)
+		return (0);
+	bytesread = fread(ch, sizeof(char), letters, file);
+	if (bytesread > 0)
+	{
+		ch[bytesread] = '\0';
+		byteswritten = printf("%s", ch);
+		if (byteswritten != bytesread)
+		{
+			fclose(file);
+			return (0);
+		}
+	}
+	fclose(file);
+	if (bytesread < letters)
+		return (bytesread);
+	else
+		return (0);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - function
+ *
+ * @filename: file name
+ *
+ * @text_content: text content
+ *
+ * Return: intger
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t bytes = 0, len = strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
+	if (len)
+		bytes = write(file, text_content, len);
+	close(file);
+	return (bytes == len ? 1 : -1);
+}
