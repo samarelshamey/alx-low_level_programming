94a57bc89fb028344cffc2fd46b5744bb4fdc8f7
samarelshamey <smsmelshamey90@gmail.com>
2023-10-30 05:12:33 -0700

    file i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -36,9 +36,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	file = open(filename, O_WRONLY | O_APPEND);
-        if (file == -1)
-                return (-1);
-        if (len)
-                byte = write(file, text_content, len);
-        close(file);
-        return (byte == len ? 1 : -1);
+	if (file == -1)
+		return (-1);
+	if (len)
+		byte = write(file, text_content, len);
+	close(file);
+	return (byte == len ? 1 : -1);
+}
