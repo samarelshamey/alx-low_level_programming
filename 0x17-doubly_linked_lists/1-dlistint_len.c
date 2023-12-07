f1c889c21e743344a3af8152ef9e2bb4c1e07d4a
root <smsmelshamey90@gmail.com>
2023-12-07 10:23:05 -0800

    doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return number of element in list
+ *
+ * @h: head
+ * Return: integer
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	int count = 0;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count)
+}
