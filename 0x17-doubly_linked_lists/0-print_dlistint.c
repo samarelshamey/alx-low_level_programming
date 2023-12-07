c939d8f62e401dbe784def0a87e9121006384339
root <smsmelshamey90@gmail.com>
2023-12-07 10:12:43 -0800

    doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print list
+ * @h: head
+ * Return: integer
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *ptr;
+	int count = 0;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
