d00aab7ec3981a138501d4122ac6136c0530102e
root <smsmelshamey90@gmail.com>
2023-12-07 13:06:29 -0800

    list and tuple

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,16 +29,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = *h;
-		(*h)->prev = new;
-		*h = new;
-		return (*h);
+		return (add_dnodeint(h, n));
 	}
 	while (pos < idx - 1)
 	{
 		if (ptr1 == NULL)
 		{
-			free(new);
 			return (NULL);
 		}
 		ptr1 = ptr1->next;
@@ -47,7 +43,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	ptr2 = ptr1->next;
 	new->prev = ptr1;
 	ptr1->next = new;
-	if (ptr2 != NULL)
+	if (ptr2 == NULL)
+	{
+		return  (add_dnodeint_end(h, n));
+	}
+	else
 	{
 		new->next = ptr2;
 		ptr2->prev = new;
