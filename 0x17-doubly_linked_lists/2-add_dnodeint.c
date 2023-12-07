3bfa0654dd35503163c1bfca1830f6a888c16ec6
root <smsmelshamey90@gmail.com>
2023-12-07 10:39:10 -0800

    doubly linked list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node at the beggining of list
+ *
+ * @head: head
+ * @n: data
+ * Return: head
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp;
+
+	temp = malloc(sizeof(dlistint_t));
+	temp->prev = NULL;
+	temp->n = n;
+	temp->next = NULL;
+
+	temp->next = *head;
+	(*head)->prev = temp;
+	*head = temp;
+	return (*head);
+}
