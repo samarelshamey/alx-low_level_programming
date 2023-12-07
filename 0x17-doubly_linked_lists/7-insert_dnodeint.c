7d497296e4c62b0dd196f4ef8561b11e3ada0e65
root <smsmelshamey90@gmail.com>
2023-12-07 12:52:41 -0800

    list and tuple

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert node at spec index
+ *
+ * @h: head
+ * @idx: index
+ * @n: data
+ * Return: head
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t ptr1 = *h, ptr2 = NULL;
+	unsigned int pos = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->prev = NULL;
+	new->n = n;
+	new->next = NULL;
+
+	if (id == 0)
+	{
+		new->next = *h;
+		(*h)->prev = new;
+		*h = new;
+		return (*h);
+	}
+	while (pos < idx)
+	{
+		if (ptr1 == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		ptr1 = ptr1->next;
+		pos++;
+	}
+	ptr2 = ptr1->next;
+	new->prev = ptr1;
+	ptr1->next = new;
+	if (ptr2 != NULL)
+	{
+		new->next = ptr2;
+		ptr2->prev = new;
+	}
+	return (*h)
+}
