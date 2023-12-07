7d77cafe6fbc1124c2c2107c805971dc15b2d3d0
root <smsmelshamey90@gmail.com>
2023-12-07 11:34:55 -0800

    doubly linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - return node at spec index
+ * @head: head
+ * @index: node index
+ * Return: node
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr = head;
+	unsigned int pos = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (pos < index)
+	{
+		ptr = ptr->next;
+		pos++;
+	}
+	return (ptr);
+}
