95c0391e912c35e66acb44ff717fd0c6090a02c7
root <smsmelshamey90@gmail.com>
2023-12-07 10:45:21 -0800

    doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	temp->next = NULL;
 
 	temp->next = *head;
-	(*head)->prev = temp;
+	if (*head != NULL)
+	{
+		(*head)->prev = temp;
+	}
 	*head = temp;
 	return (temp);
 }
