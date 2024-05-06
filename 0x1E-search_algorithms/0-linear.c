e9c3473dd749f316b6d5a020f5932a54a4121434
root <smsmelshamey90@gmail.com>
2024-05-06 03:43:37 -0700

    search algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ * linear_search - func search for value in array
+ * @array: pointer to the first element of the array
+ * @size: array size
+ * @value: value to search for
+ * Return: integer
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+/**
+ * binary_search - func search for value in array
+ * @array: pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: integer
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l = 0, r = size -1, mid, i;
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (r > l)
+	{
+		mid = (l + r ) / 2;
+		printf("Searching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			printf("%d", array[i]);
+			if (i < r)
+				printf(", ");
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return mid;
+		else if (array[mid] < value)
+			l = mid + 1;
+		else
+			r = mid - 1;
+	}
+	return -1;
+}
